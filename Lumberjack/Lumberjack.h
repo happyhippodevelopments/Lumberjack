7887d59f86949ff170ec699bc1080d6551e488f0
happyhippodevelopments <RGMAC@Raouls-iMac.local>
2017-04-02 16:19:44 -0400

    Publish

---
+++ Lumberjack/Lumberjack.h
@@ -0,0 +1,19 @@
+//
+//  Lumberjack.h
+//  Lumberjack
+//
+//  Created by Raoul Gatzen on 4/2/17.
+//  Copyright © 2017 happyhippo developments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lumberjack.
+FOUNDATION_EXPORT double LumberjackVersionNumber;
+
+//! Project version string for Lumberjack.
+FOUNDATION_EXPORT const unsigned char LumberjackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lumberjack/PublicHeader.h>
+
+
